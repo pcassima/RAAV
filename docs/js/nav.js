7b263ece95f6a5376445c38398fe36725292dba1
Pieter-Jan Cassiman <pieterjan.cassiman@gmail.com>
2019-05-12 11:30:43 +0200

    Create nav.js

---
+++ docs/js/nav.js
@@ -0,0 +1,8 @@
+function hamburgerNav() {
+    let element = document.getElementById("nav-list");
+    if (element.classList.contains("active")) {
+        element.classList.remove("active");
+    } else {
+        element.classList.add("active");
+    }
+}
